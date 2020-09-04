82e1a7fd03b76eef61b461c361f76a8fecf1b90a
rabelsan <rabelsan@gmail.com>
2020-09-04 22:01:51 +0200

    create nodepop

---
+++ models/Advertisement.js
@@ -0,0 +1,29 @@
+'use strict';
+
+const mongoose = require('mongoose');
+
+// crear un esquema (https://mongoosejs.com/docs/schematypes.html)
+const advertisementSchema = mongoose.Schema({
+  name: { type: String, required:true, index: true},
+  sale: {type: Boolean, required: true},
+},
+  {
+    autoIndex: process.env.NODE_ENV !== 'production', // no crear los índices automáticamente en producción (los crearé yo cuando me convenga)
+  }
+);
+
+// método estático
+agenteSchema.statics.lista = function(filtro, limit, skip, sort, fields) {
+  const query = Agente.find(filtro);
+  query.limit(limit);
+  query.skip(skip);
+  query.sort(sort);
+  query.select(fields);
+  return query.exec();
+}
+
+// crear el modelo
+const Agente = mongoose.model('Agente', agenteSchema);
+
+// exportar el modelo
+module.exports = Agente;
